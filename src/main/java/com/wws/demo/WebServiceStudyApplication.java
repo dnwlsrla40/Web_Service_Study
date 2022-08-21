0d822a33d7c0744f11a526844991cb5d814710d7
DESKTOP-PEE814T\hong <towijin2100@gmail.com>
2022-08-21 14:24:04 +0900

    [commit setting] -

---
+++ src/main/java/com/wws/demo/WebServiceStudyApplication.java
@@ -0,0 +1,13 @@
+package com.wws.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServiceStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wws/demo/WebServiceStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wws.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
