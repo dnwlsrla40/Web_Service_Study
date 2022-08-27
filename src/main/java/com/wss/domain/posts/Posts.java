05c911cea4bda6bdd44e19d61ea7b1c84d7340f2
DESKTOP-K9D48KS\mycom <dnwlsrla40@naver.com>
2022-08-27 10:01:56 +0900

    [Insert.김우진] 2주차. p96까지 진행완료

---
+++ src/main/java/com/wss/domain/posts/Posts.java
@@ -0,0 +1,32 @@
+package com.wss.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)            // @Column이 없어도 매핑 됨, 주로 기본값 외에 추가로 변경사항이 있을 경우 사용
+    private String content;
+
+    private String author;
+
+    @Builder                                                        // 생성자 상단에 선언 시 생성자에 포함된 필드만 빌더에 포함
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+}
---
+++ src/main/java/com/wss/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.wss.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
